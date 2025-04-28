510cb7f9964d7814275c7c175e0ed4482d633652
chelseabhuiyan <bhuiyanfchelsea@gmail.com>
2025-04-28 01:11:13 -0400

    Implemented Homescreen options and created the ledger.java file

---
+++ src/main/java/com/pluralsight/Ledger.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Ledger {
+    public static void addDeposit() {
+        // Logic to add a deposit will go here
+    }
+
+    public static void makePayment() {
+        // Logic to make a payment will go here
+    }
+
+    public static void viewLedger() {
+        // Logic to view the ledger will go here
+    }
+}
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,42 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean runWhileLoop=true;
+
+        while (runWhileLoop) {
+            System.out.println(); //Leaves a blank line in between
+            System.out.println("Welcome to the Accounting Ledger App!");
+            System.out.println("D. Add Deposit");
+            System.out.println("P. Make Payment (Debit)");
+            System.out.println("L. Ledger");
+            System.out.println("X. Exit");
+            System.out.print("Choose an option: ");  /*asks user for input on the same line*/
+
+            String choice = scanner.nextLine().trim().toUpperCase();  /*stores the user input as the value for variable choice*/
+
+            switch (choice) {
+                case "D" -> {
+                    Ledger.addDeposit();
+                }
+                case "P" -> {
+                    Ledger.makePayment();
+                }
+                case "L" -> {
+                    Ledger.viewLedger();
+                }
+                case "X" -> {
+                    System.out.println("Exiting application. Goodbye!");
+                    runWhileLoop = false; //Exits while loop
+                }
+                default ->
+                        System.out.println("Invalid input. Please enter D, P, L, or X."); /*if anything other than D,P,L, or X is typed it returns this message to the user*/
+            }
+        }
+        scanner.close(); //Close Scanner
     }
 }
